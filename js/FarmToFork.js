042b0cd4774d1913195d42458bf00593da76e801
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-01-08 12:08:38 -0700

    Finishing core functionality

--- js/FarmToFork.js
+++ js/FarmToFork.js
@@ -1,95 +1,56 @@
-// Require dependencies
 const driver = require('bigchaindb-driver')
 const bip39 = require('bip39')
-
-// Require the dotenv library
 require('dotenv').config()
 
 class FarmToFork {
-
-  /**
-   * Initialize a new class that we'll use to handle our connection with the network
-   */
   constructor(keySeed) {
-    // Initialize a new connection
-    this.connection = new driver.Connection(process.env.APP_UR)
-    // Not sure app_id and app_key are needed anymore
-    // this.connection = new driver.Connection(process.env.APP_URL, {
-    //   app_id: process.env.APP_ID,
-    //   app_key: process.env.APP_KEY
-    // })
-
-    // Initialize an identity
+    this.connection = new driver.Connection(process.env.APP_URL, {
+      app_id: process.env.APP_ID,
+      app_key: process.env.APP_KEY
+    })
     this.currentIdentity = this.generateKeypair("ftf")
   }
 
-  // Here we'll write our methods
-
-  /**
-   * Generate a keypair based on the supplied seed string
-   * @param {string} keySeed - The seed that should be used to generate the keypair
-   * @returns {*} The generated keypair
-   */
   generateKeyPair(keySeed) {
     if (typeof keySeed = "undefined" || keySeed = "") return new driver.Ed25519Keypair()
     return new driver.Ed25519Keypair(bip39.mnemonicToSeed(keySeed).slice(0, 32))
   }
 
   createAsset(foodItem) {
-    
+
     return new Promise((resolve, reject) => {
-    
-      // Create asset object
       const assetData = {
         "type": "FtfTutorialAsset",
         "item": foodItem
       }
 
-      // Create metadata object
       const metaData = {
         "action": "Introduced",
         "data": new Date().toISOString()
       }
-    
-      // Create a CREATE transaction
+
       const introduceFoodItemToMarketTransaction = driver.makeCreateTransaction(
-      
-        // Include the foodItem as asset data
+
         assetData,
-        // Include metadata to give information on the action
         metData,
-        // Create an output
         [driver.Transaction.makeOutput(
           driver.Transacction.makeEd25519Condition(this.currentIdentity.publickKey))]
-        // Include the public key
         this.currentIdentity.publicKey
       )
 
-      // We sign the transaction
       const signedTransaction = driver.Transaction.signTransaction(introduceFoodItemToMarketTransaction, this.currentIdentity.privateKey)
 
-      // Post the transaction to the network
       this.connection.postTransactionCommit(signedTransaction).then(postedTransaction => {
-
-        // Let the promise resolve the created transaction
         resolve(postedTransaction)
 
-        // Catch exceptions
       }).catch(err => {
         reject(new Error(err))
       })
     })
   }
 
-  /**
-   * Get a list of ids of unspent transactions for a certain public key
-   * @returns {Array} An array containing all unspent transactions for a certain public key
-   */
   getAssets() {
-
     return new Promise((resolve, reject) => {
-    
-      // Get a list of ids of unspent transactions from a public key
       this.connection.listOutputs(this.currentIdentity.publicKey, false).then(response => {
         resolve(response)
       }).catch(err => {
@@ -98,34 +59,65 @@ class FarmToFork {
     })
   }
 
-  /**
-   * Update the asset by issuing a TRANSFER transaction with metadata containing the action performed on the asset
-   * @param {*} transaction - The transaction that needs to be chained upon
-   * @param {*} action - The action performed on the asset (e.g. processed with preservatives)
-   */
   updateAsset(transaction, action) {
 
     return new Promise((resolve, reject) => {
-
       console.log(transaction)
-
-      // Create metadata for action
       const metaData = {
         "action": action,
-        "data": new Date().toISOString()
+        "date": new Date().toISOString()
       }
 
-      // Create a new TRANSFER transaction
       const updateAssetTransaction = driver.Transaction.makeTransferTransaction(
-        
-        // previous transaction
+        [{ tx: transaction, output_index: 0 }],
+        [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(this.currentIdentity.publicKey))],
+        metaData
       )
+
+      const signedTransaction = driver.Transaction.signTransaction(updateAssetTransaction, this.currentIdentity.privateKey)
+
+      console.log("Posting transaction.")
+      this.connection.postTransactionCommit(signedTransaction).then(postedTransaction => {
+        resolve(postedTransaction)
+      }).catch(err => {
+        reject(err)
+      })
     })
   }
 
+  transferAsset(transaction, receiverPublicKey) {
+    return new Promise((resolve, reject) => {
+      const metaData = {
+        "action" : "Transfer food-item to another firm.",
+        "date" : new Date().toISOString()
+      }
+      const transferTransaction = driver.Transaction.makeTransferTransaction(
+        [{ tx: transaction, output_index: 0}],
+        [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(receiverPublicKey))],
+        metaData
+      )
+      const signedTransaction = driver.Transaction.signTransaction(transferTransaction, this.currentIdentity.privateKey)
+      this.connection.postTransaction(signedTransaction).then( postedTransaction => {
+        return this.connection.pollStatusAndFetchTransaction(postedTransaction.id)
+      }).then( successfullyPostedTransaction => {
+        resolve(successfullyPostedTransaction)
+      }).catch( error => {
+        reject(error)
+      })
+    })
+  }
+
+  getAllAssets() {
+    return new Promise((resolve, reject) => {
+      this.connection.searchAssets('FtfTutorialAsset').then( response => {
+        resolve(response)
+      })
+    }).catch(error => {
+      reject(error)
+    })
+  }
 }
 
-// Create exports to make some functionality available in the browser
 module.exports = {
   FarmToFork
 }
